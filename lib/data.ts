55303d6eb610820a45026c9f5409183e6d1cb974
LiramVahady <66430729+LiramVahady@users.noreply.github.com>
2026-06-02 14:23:36 +0300

    Remove microblading-9 from home carousel

--- lib/data.ts
+++ lib/data.ts
@@ -75,13 +75,11 @@ export const services: Service[] = [
       '/microblading-2.webp',
       '/microblading-8.webp',
       '/microblading-5.webp',
-      '/microblading-9.webp',
     ],
     homeImagePositions: [
       '50% 40%',
       '50% 35%',
       '50% 35%',
-      '50% 25%',
     ],
     features: [
       'תוצאות המחזיקות עד שנה',
